7d7c2851c98af889cd504f4368a916532fa393fa
Eduardo Venegas <evenegas06@hotmail.com>
2023-07-25 23:55:04 -0600

    create pages and routes for the app... #400

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Clients from "./pages/Clients";
+import Products from "./pages/Products";
+import Orders from "./pages/Orders";
+
 import Header from "./components/layouts/Header";
 import Sidebar from "./components/layouts/Sidebar";
 
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <Header />
             <div className="grid contenedor contenido-principal">
                 <Sidebar />
 
                 <main className="caja-contenido col-9">
-                    {/* TODO: Routing */}
+                    <Routes>
+                        <Route path="/" element={<Clients />} />
+                        <Route path="/productos" element={<Products />}/>
+                        <Route path="/pedidos" element={<Orders />}/>
+                    </Routes>
                 </main>
             </div>
-        </>
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/Clients.jsx
@@ -0,0 +1,6 @@
+const Clients = () => {
+    return (
+        <h2>Clients</h2>
+    );
+};
+export default Clients;
