b258642a96f680509f0d6e7ecedb0d5c6ccda8e4
Eduardo Venegas <evenegas06@hotmail.com>
2023-12-27 00:24:27 -0600

    add new products... #425

--- src/pages/NewProductForm.jsx
+++ src/pages/NewProductForm.jsx
@@ -1,4 +1,9 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Swal from 'sweetalert2';
+
+import axios_instance from '../utils/axios_instance';
 
 const NewProductForm = () => {
 	/* ----- State ----- */
@@ -7,6 +12,51 @@ const NewProductForm = () => {
 		price: 0,
 	});
 	const [image, setImage] = useState(null);
+
+	/* ----- Hooks ----- */
+	const navigate = useNavigate();
+
+	/**
+	 * Add new product into DB by API.
+	 *
+	 * @param {Object} event
+	 */
+	const addProduct = async (event) => {
+		event.preventDefault();
+
+		// Create formData
+		const form_data = new FormData();
+
+		form_data.append('name', product.name);
+		form_data.append('price', product.price);
+		form_data.append('image', image);
+
+		try {
+			const response = await axios_instance.post('/productos', form_data, {
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+			});
+
+			if (response.status == 200) {
+				Swal.fire(
+					'Producto agregado correctamente',
+					response.data.message,
+					'success'
+				);
+			}
+
+			navigate('/productos');
+		} catch (error) {
+			console.error(error);
+
+			Swal.fire({
+				type: 'error',
+				title: 'Lo sentimos, hubo un error inesperado.',
+				text: 'Vuelva a intentarlo mas tarde.',
+			});
+		}
+	};
 
 	/**
 	 * Fill *product state* attributes with the input value.
@@ -35,7 +85,7 @@ const NewProductForm = () => {
 		<>
 			<h2>Nuevo Producto</h2>
 
-			<form>
+			<form onSubmit={addProduct}>
 				<legend>Llena todos los campos</legend>
 
 				<div className="campo">
@@ -43,7 +93,7 @@ const NewProductForm = () => {
 					<input
 						type="text"
 						placeholder="Nombre Producto"
-						name="nombre"
+						name="name"
 						onChange={handleInputChange}
 					/>
 				</div>
@@ -52,7 +102,7 @@ const NewProductForm = () => {
 					<label>Precio:</label>
 					<input
 						type="number"
-						name="precio"
+						name="price"
 						min="0.00"
 						step="0.01"
 						placeholder="Precio"
@@ -64,7 +114,7 @@ const NewProductForm = () => {
 					<label>Imagen:</label>
 					<input
 						type="file"
-						name="imagen"
+						name="image"
 						onChange={handleFile}
 					/>
 				</div>
